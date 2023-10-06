a5e6b986b21661bd5f50332edd17782e237c083f
Thomas Gillis <thoma.gillis@gmail.com>
2023-10-06 05:58:15 +0200

    common: add mlx runtime and allow OFI_DIR to be overwritten

--- make_arch/default.mak
+++ make_arch/default.mak
@@ -9,30 +9,30 @@ USE_CUDA ?=1
 CC=gcc
 CXX=g++
 NVCC=nvcc
-PMI_DIR=${HOME}/lib-OFI-1.18.0-CUDA-11.7.0-dbg
-OFI_DIR=${HOME}/lib-OFI-1.18.0-CUDA-11.7.0-dbg
+PMI_DIR?=${HOME}/lib-OFI-1.18.0-CUDA-11.7.0-dbg
+OFI_DIR?=${HOME}/lib-OFI-1.18.0-CUDA-11.7.0-dbg
 #---------------------------------------------------------------------------------------------------
 # LUMI
 else ifneq (,$(or $(findstring uan,$(HOSTNAME)),$(findstring nid,$(HOSTNAME))))
 USE_CUDA ?=0
 CC=gcc
 CXX=g++
-PMI_DIR=${HOME}/lib-PMI-4.1.1
-OFI_DIR=/opt/cray/libfabric/1.15.2.0
-OFI_LIB=/opt/cray/libfabric/1.15.2.0/lib64
+PMI_DIR?=${HOME}/lib-PMI-4.1.1
+OFI_DIR?=/opt/cray/libfabric/1.15.2.0
+OFI_LIB?=/opt/cray/libfabric/1.15.2.0/lib64
 #---------------------------------------------------------------------------------------------------
 # MELUXINA
 else ifneq (,$(findstring mel,$(HOSTNAME)))
 CC=gcc
 CXX=g++
-PMI_DIR=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
+PMI_DIR?=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
 #OFI_DIR=${HOME}/libfabric/_inst
-OFI_DIR=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
+OFI_DIR?=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
 #---------------------------------------------------------------------------------------------------
 else
 USE_CUDA ?=0
 CC=clang
-PMI_DIR=/Users/tgillis/dbs_lib/lib_OFI-1.18.1-dbg
-OFI_DIR=/Users/tgillis/dbs_lib/lib_OFI-1.18.1-dbg
-ARGP_DIR=/opt/homebrew
+PMI_DIR?=/Users/tgillis/dbs_lib/lib_OFI-1.18.1-dbg
+OFI_DIR?=/Users/tgillis/dbs_lib/lib_OFI-1.18.1-dbg
+ARGP_DIR?=/opt/homebrew
 endif
--- run/mlx.sh
+++ run/mlx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --partition=gpu
 #SBATCH --time=0:10:00
-#SBATCH --ntasks=4
+#SBATCH --ntasks=64
 #SBATCH --nodes=2
 #SBATCH --account=p200210
 #SBATCH --qos=short
@@ -14,14 +14,15 @@ module load UCX
 module load Automake Autoconf
 module load libtool
 module load Perl
-module load CUDA
+module load CUDA GDRCopy
 module list
 
 #-------------------------------------------------------------------------------
 # get a unique tag
 TAG=`date '+%Y-%m-%d-%H%M'`-`uuidgen -t | head -c 4`
 # get the dir list
-MPI_DIR=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
+MPI_DIR=${HOME}/lib-OFI-1.19.0-CUDA-11.7.0
+#MPI_DIR=${HOME}/lib-OFI-1.18.1-CUDA-11.7.0
 HOME_DIR=${HOME}/rmem
 SCRATCH_DIR=/project/scratch/p200210/tgillis/benchme_${TAG}_${SLURM_JOBID}
 
@@ -38,6 +39,11 @@ cp -r ${HOME_DIR}/make_arch .
 cp -r ${HOME_DIR}/Makefile .
 
 #-------------------------------------------------------------------------------
+# asan with cuda, from https://github.com/google/sanitizers/issues/629
+# asan with pthread, from https://github.com/google/sanitizers/issues/1171
+export ASAN_OPTIONS=protect_shadow_gap=0:use_sigaltstack=0
+#-------------------------------------------------------------------------------
+export FI_HMEM_CUDA_USE_GDRCOPY=1
 export FI_VERBS_DEVICE_NAME=mlx5_0
 #export FI_VERBS_RX_SIZE=2048
 #export FI_VERBS_TX_SIZE=2048
@@ -45,47 +51,32 @@ export FI_VERBS_DEVICE_NAME=mlx5_0
 #export FI_OFI_RXM_TX_SIZE=2048
 #export FI_LOG_LEVEL=Debug
 #export FI_VERBS_PREFER_XRC=1
+export FI_OFI_RXM_SAR_LIMIT=192
+export FI_OFI_RXM_BUFFER_SIZE=192
+export PSM3_PRINT_STATS=1
+export PSM3_PRINT_STATS_HELP=1
 #-------------------------------------------------------------------------------
-MPI_OPT="-n 2 -ppn 1 -l --bind-to core"
+# need to put 2 here to let some space of the thread!
+MPI_OPT="-n 2 -ppn 1 -l --bind-to core:2"
 #-------------------------------------------------------------------------------
-export USE_CUDA=0
-make info
-make clean
-#OPTS="-ldl -rdynamic" make fast
-make fast
-ldd rmem
 
-#test delivery
-declare -a test=(
-    "-r am -d am -c delivery"
-    "-r am -d am -c cq_data"
-    "-r tag -d tag -c cq_data"
-)
-for RMEM_OPT in "${test[@]}"; do
-    echo "==> ${MPI_OPT} with ${RMEM_OPT}"
-    FI_PROVIDER="verbs;ofi_rxm" ${MPI_DIR}/bin/mpiexec ${MPI_OPT} ./rmem ${RMEM_OPT}
+for device in 0 1; do
+    export USE_CUDA=${device}
+    make clean
+    OFI_DIR=${HOME}/lib-OFI-1.19.0-CUDA-11.7.0 make info fast
+    ldd rmem
+    #test delivery
+    declare -a test=(
+        "-r am -d am -c delivery"
+        "-r am -d am -c cq_data"
+        "-r tag -d tag -c cq_data"
+    )
+    for RMEM_OPT in "${test[@]}"; do
+        echo "==> ${MPI_OPT} with ${RMEM_OPT} - CUDA? ${USE_CUDA}"
+        FI_PROVIDER="verbs;ofi_rxm" ${MPI_DIR}/bin/mpiexec ${MPI_OPT} ./rmem ${RMEM_OPT}
+        #FI_PROVIDER="psm3" ${MPI_DIR}/bin/mpiexec ${MPI_OPT} ./rmem ${RMEM_OPT}
+    done
 done
-
-##-------------------------------------------------------------------------------
-## test rtr and dtc
-#declare -a test=(
-#    "-r am -d am -c delivery"
-#    "-r tag -d tag -c delivery")
-#for RMEM_OPT in "${test[@]}"; do
-#    echo "==> ${MPI_OPT} with ${RMEM_OPT}"
-#    FI_PROVIDER="psm3" ${MPI_DIR}/bin/mpiexec ${MPI_OPT} ./rmem ${RMEM_OPT}
-#done
-#
-##test delivery
-#declare -a test=(
-#    "-r am -d am -c delivery"
-#    "-r am -d am -c counter"
-#    "-r am -d am -c cq_data")
-#for RMEM_OPT in "${test[@]}"; do
-#    echo "==> ${MPI_OPT} with ${RMEM_OPT}"
-#    FI_PROVIDER="psm3" ${MPI_DIR}/bin/mpiexec ${MPI_OPT} ./rmem ${RMEM_OPT}
-#done
-#
 
 ##-------------------------------------------------------------------------------
 #FI_PROVIDER="psm3" ${MPI_DIR}/bin/mpiexec -ppn 1 -l --bind-to core \
